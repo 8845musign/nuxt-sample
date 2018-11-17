626bf1cab47451165cbc751872df3a3d7cc7c69c
8845musign <8845musign@gmail.com>
2018-11-17 16:36:30 +0900

    first commit

---
+++ components/NoriCom.vue
@@ -0,0 +1,59 @@
+<template lang="html">
+  <div class="sample">
+    Hello, I'm {{ name }}
+    reverse: {{ reverseName }}
+    num: {{ num }}
+    <button 
+      type="button" 
+      @click="plus">+</button>
+
+    <div 
+      :class="['hide', seven]">
+      seven times click disappear
+    </div>
+    <h1>{{ title }}</h1>
+    <slot />
+  </div>
+</template>
+​
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: 'Defalut Title'
+    }
+  },
+  data() {
+    return {
+      name: 'hiroki',
+      num: 0
+    }
+  },
+  computed: {
+    reverseName() {
+      return this.name
+        .split('')
+        .reverse()
+        .join('')
+    },
+    seven() {
+      return this.num === 7 ? 'seven' : null
+    }
+  },
+  methods: {
+    plus() {
+      this.num++
+    }
+  }
+}
+</script>
+​
+<style lang="scss" scoped>
+.hide {
+  display: none;
+  &.seven {
+    display: block;
+  }
+}
+</style>
---
+++ pages/about.vue
@@ -0,0 +1,23 @@
+<template lang="html">
+  <div>
+    <nuxt-link to="/">TOP</nuxt-link>
+  </div>
+</template>
+​
+<script>
+export default {
+  head() {
+    return {
+      // タイトルテンプレートがある場合は "ABOUT - ポートフォリオ" になる
+      title: 'ABOUT',
+      meta: [
+        { hid: 'description', name: 'description', content: 'About ページ' }
+      ]
+    }
+  },
+  layout: 'dark'
+}
+</script>
+​
+<style lang="scss">
+</style>
